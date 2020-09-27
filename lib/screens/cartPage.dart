75c2bdae688be8c4a0fa4da06129825767167269
sanjay kumar <36959490+sanjaysans@users.noreply.github.com>
2020-09-27 18:43:02 +0530

    Cart amount

--- lib/screens/cartPage.dart
+++ lib/screens/cartPage.dart
@@ -1,6 +1,7 @@
 import 'package:canteen_food_ordering_app/apis/foodAPIs.dart';
 import 'package:canteen_food_ordering_app/models/cart.dart';
 import 'package:canteen_food_ordering_app/notifiers/authNotifier.dart';
+import 'package:canteen_food_ordering_app/widgets/customRaisedButton.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -11,6 +12,9 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
+
+  double sum = 0;
+  int itemsCount = 0;
 
   @override
   void initState() {
@@ -76,48 +80,71 @@ class _CartPageState extends State<CartPage> {
             _cartItems.add(Cart(item.documentID, count[item.documentID], item.data['item_name'], item.data['total_qty'], item.data['price']));
           });
           if (_cartItems.length > 0){
+            sum = 0;
+            itemsCount= 0;
+            _cartItems.forEach((element) {
+              if(element.price != null && element.count != null){
+                sum += element.price * element.count;
+                itemsCount += element.count;
+              }
+            });
             return Container(
               margin: EdgeInsets.only(top: 10.0),
-              child:ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: _cartItems.length,
-                itemBuilder: (context, int i) {
-                return ListTile(
-                  title: Text(_cartItems[i].itemName ?? ''),
-                  subtitle: Text('cost: ${_cartItems[i].price.toString()}'),
-                  trailing: Row(      
-                    mainAxisSize: MainAxisSize.min,    
-                    children: <Widget>[
-                    (_cartItems[i].count == null || _cartItems[i].count <= 1) ? 
-                    IconButton(
-                      onPressed: () async{
-                        setState(() {
-                          foodIds.remove(_cartItems[i].itemId);
-                        });
-                        await editCartItem(_cartItems[i].itemId, 0, context);
-                      },
-                      icon: new Icon(Icons.delete),
-                    )
-                    : IconButton(
-                      onPressed: () async{
-                        await editCartItem(_cartItems[i].itemId, (_cartItems[i].count-1), context);
-                      },
-                      icon: new Icon(Icons.remove),
-                    ),
-                    Text(
-                      '${_cartItems[i].count ?? 0}',
-                      style: TextStyle(fontSize: 18.0),
-                    ),
-                    IconButton(
-                      icon: new Icon(Icons.add),
-                      onPressed: () async{
-                        await editCartItem(_cartItems[i].itemId, (_cartItems[i].count+1), context);
-                      },
-                    )
-                  ]),
-                );
-              }),
+              child: Column(
+                children: [
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: _cartItems.length,
+                    itemBuilder: (context, int i) {
+                    return ListTile(
+                      title: Text(_cartItems[i].itemName ?? ''),
+                      subtitle: Text('cost: ${_cartItems[i].price.toString()}'),
+                      trailing: Row(      
+                        mainAxisSize: MainAxisSize.min,    
+                        children: <Widget>[
+                        (_cartItems[i].count == null || _cartItems[i].count <= 1) ? 
+                        IconButton(
+                          onPressed: () async{
+                            setState(() {
+                              foodIds.remove(_cartItems[i].itemId);
+                            });
+                            await editCartItem(_cartItems[i].itemId, 0, context);
+                          },
+                          icon: new Icon(Icons.delete),
+                        )
+                        : IconButton(
+                          onPressed: () async{
+                            await editCartItem(_cartItems[i].itemId, (_cartItems[i].count-1), context);
+                          },
+                          icon: new Icon(Icons.remove),
+                        ),
+                        Text(
+                          '${_cartItems[i].count ?? 0}',
+                          style: TextStyle(fontSize: 18.0),
+                        ),
+                        IconButton(
+                          icon: new Icon(Icons.add),
+                          onPressed: () async{
+                            await editCartItem(_cartItems[i].itemId, (_cartItems[i].count+1), context);
+                          },
+                        )
+                      ]),
+                    );
+                  }),
+                  Text("Total ($itemsCount items): $sum INR"),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: CustomRaisedButton(buttonText: 'Proceed to buy'),
+                  ),
+                  SizedBox(
+                    height: 70,
+                  ),
+                ],
+              )
             );
           } else {
             return Container(
